b3bcdf7e9d63662e892db21c15d7b4547b71cf0e
Kwan Rimorin John Eric <je_macbook@JE-Macbook.local>
2022-08-17 23:16:29 +0800

    home icon

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -134,7 +134,16 @@ function Home({ postalcode, name }: homeProps) {
     <>
       <Navbar bg="light" expand="sm">
         <Container fluid>
-          <Navbar.Brand>{name}</Navbar.Brand>
+          <Navbar.Brand>
+            <img
+              alt=""
+              src={`${process.env.PUBLIC_URL}/favicon-32x32.png`}
+              width="32"
+              height="32"
+              className="d-inline-block align-top"
+            />{" "}
+            {name}
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse
             id="basic-navbar-nav"
