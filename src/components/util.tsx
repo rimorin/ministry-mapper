acd9c2643f63c3d71726a31f99454714e903da3b
Kwan Rimorin John Eric <je_macbook@JE-Macbook.local>
2022-09-06 08:15:24 +0800

    Proper login error handling

--- src/components/util.tsx
+++ src/components/util.tsx
@@ -75,6 +75,18 @@ const ModalUnitTitle = ({ unit, floor, postal }: TitleProps) => {
   );
 };
 
+const errorMessage = (code: String) => {
+  if (code === "auth/too-many-requests")
+    return "Device has been blocked temporarily. Please try again later.";
+  if (code === "auth/user-disabled")
+    return "Account disabled. Please contact support.";
+  if (code === "auth/web-storage-unsupported")
+    return "Your browser does not support web storage. Please enable it and try again";
+  if (code === "auth/network-request-failed")
+    return "Network error. Please either try again with a stable internet connection or contact support";
+  return "Invalid Credentials";
+};
+
 const ZeroPad = (num: String, places: number) => num.padStart(places, "0");
 
 const assignmentMessage = (address: String) => {
@@ -200,6 +212,7 @@ export {
   getMaxUnitLength,
   getCompletedPercent,
   addHours,
+  errorMessage,
   NavBarBranding,
   Legend,
   STATUS_CODES,
