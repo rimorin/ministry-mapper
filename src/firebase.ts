160c4eb0c259c770723bc46c7122867ad144a788
Kwan Rimorin John Eric <je_macbook@JE-Macbook.local>
2022-10-04 21:02:58 +0800

    Implement appcheck

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,7 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { initializeAuth, browserLocalPersistence } from "firebase/auth";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
+declare global {
+  // var must be used for global scopes
+  // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-4.html#type-checking-for-globalthis
+  // eslint-disable-next-line no-var
+  var FIREBASE_APPCHECK_DEBUG_TOKEN: boolean | string | undefined;
+}
 const {
   REACT_APP_FIREBASE_API_KEY,
   REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -10,8 +17,12 @@ const {
   REACT_APP_FIREBASE_PROJECT_ID,
   REACT_APP_FIREBASE_BUCKET,
   REACT_APP_FIREBASE_SENDER_ID,
-  REACT_APP_FIREBASE_APP_ID
+  REACT_APP_FIREBASE_APP_ID,
+  REACT_APP_FIREBASE_RECAPTCHA_PUBLIC_KEY,
+  REACT_APP_FIREBASE_APPCHECK_DEBUG_TOKEN
 } = process.env;
+
+global.FIREBASE_APPCHECK_DEBUG_TOKEN = REACT_APP_FIREBASE_APPCHECK_DEBUG_TOKEN;
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -34,4 +45,14 @@ const auth = initializeAuth(app, {
   persistence: browserLocalPersistence
   // No popupRedirectResolver defined
 });
+
+if (REACT_APP_FIREBASE_RECAPTCHA_PUBLIC_KEY) {
+  initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider(REACT_APP_FIREBASE_RECAPTCHA_PUBLIC_KEY),
+    // Optional argument. If true, the SDK automatically refreshes App Check
+    // tokens as needed.
+    isTokenAutoRefreshEnabled: true
+  });
+}
+
 export { database, auth };
