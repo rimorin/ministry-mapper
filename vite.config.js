4250b9aad5e8fe3e646678ad07baeb0b0b3bd240
Kwan Rimorin John Eric <je_macbook@JE-Macbook.local>
2023-09-10 21:32:28 +0800

    build: include TS checker

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import visualizer from "rollup-plugin-visualizer";
+import checker from "vite-plugin-checker";
 export default defineConfig(() => {
   return {
     build: {
@@ -27,7 +28,7 @@ export default defineConfig(() => {
       port: 3000,
       host: true
     },
-    plugins: [react(), svgr(), visualizer()],
+    plugins: [react(), svgr(), visualizer(), checker({ typescript: true })],
     test: {
       globals: true,
       environment: "jsdom",
