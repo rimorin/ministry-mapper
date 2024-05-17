fde39821e9e714d5b5bbbb1c6824f25eb82a31fa
John Eric <rimorin@gmail.com>
2024-05-18 07:09:05 +0800

    fix: removed unnecessary fadings

--- src/components/statics/invalidpage.tsx
+++ src/components/statics/invalidpage.tsx
@@ -1,22 +1,20 @@
 import { memo } from "react";
-import { Container, Fade, Card } from "react-bootstrap";
+import { Container, Card } from "react-bootstrap";
 
 const InvalidPage = memo(() => (
   <Container className="container-main">
-    <Fade appear={true} in={true}>
-      <Card className="card-main">
-        <Card.Img
-          alt="Ministry Mapper logo"
-          className="mm-logo"
-          src="/android-chrome-192x192.png"
-        />
-        <Card.Body>
-          <Card.Title className="text-center">
-            This link has expired ⌛
-          </Card.Title>
-        </Card.Body>
-      </Card>
-    </Fade>
+    <Card className="card-main">
+      <Card.Img
+        alt="Ministry Mapper logo"
+        className="mm-logo"
+        src="/android-chrome-192x192.png"
+      />
+      <Card.Body>
+        <Card.Title className="text-center">
+          This link has expired ⌛
+        </Card.Title>
+      </Card.Body>
+    </Card>
   </Container>
 ));
 
